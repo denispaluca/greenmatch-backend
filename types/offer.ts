95ff9f072d24a92e78561c3510aed31ac7e36a4e
Denis Paluca <ge37dem@mytum.de>
2022-06-24 17:45:20 +0200

    Add offer model

--- types/offer.ts
+++ types/offer.ts
@@ -4,6 +4,6 @@ import { PowerPlant } from "./powerplant";
 export interface Offer extends PowerPlant {
   supplierName: string;
   supplierWebsite: string;
+  powerplantId: string;
   powerplantName: string;
-  name: string;
 }
