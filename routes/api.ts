af1dbd28c78e27ce41df68d5b0e9d335fea37b76
Jan Martin Vogt <jm.vogt@tum.de>
2022-06-24 10:08:39 +0200

    feat: uname availability check

--- routes/api.ts
+++ routes/api.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import authRouter from './auth';
+import usernameRouter from './username';
 
 const router = express.Router();
 router.use('/auth', authRouter);
+router.use('/username', usernameRouter);
 
 export default router;
