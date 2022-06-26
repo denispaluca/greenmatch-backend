5eda3905529232ad3f97ed233b9f630b3f362a7f
Sebastian Schramm <s.schramm@tum.de>
2022-06-26 22:54:29 +0200

    feat: route to create ppa

--- controllers/stripe.ts
+++ controllers/stripe.ts
@@ -10,15 +10,25 @@ export const createCustomer = async (req: Request, res: Response) => {
     res.send(customer);
 };
 
-export const clientSecret = async (req: Request, res: Response) => {
+/* 
+* A SetupIntent guides you through the process of setting up and saving a customer's payment credentials for future payments. 
+* For example, you could use a SetupIntent to set up and save your customer's card without immediately collecting a payment. 
+* Later, you can use PaymentIntents to drive the payment flow. 
+*/
+export const setupIntent = async (req: Request, res: Response) => {
     const setupIntent = await stripe.setupIntents.create({
         payment_method_types: ['sepa_debit'],
         customer: req.body.customer,
     });
-    const clientSecret = setupIntent.client_secret;
-    res.send({ "client_secret": clientSecret });
+    res.send(setupIntent);
 };
 
+/*
+* items: the price id of the purchased ppa
+* cancel_at: unix timestamp when the subscription should stop
+* billing_cycle_anchor: unix timestamp when the subscription should start
+* proportion_behavior: do not proportionally charge the customer before the billing cycle starts
+*/
 export const subscribe = async (req: Request, res: Response) => {
     const subscription = await stripe.subscriptions.create({
         customer: req.body.customer,
@@ -30,5 +40,25 @@ export const subscribe = async (req: Request, res: Response) => {
         proration_behavior: 'none',
         default_payment_method: req.body.default_payment_method,
     });
-    console.log('Subscription Successfully Set Up');
+    console.log('Subscription successfully set up!');
+    res.send(subscription);
+};
+
+/* 
+* Creates product with price and return price object
+* unit_amount: monthly amount in cents
+*/
+export const ppa = async (req: Request, res: Response) => {
+    // create product
+    const product = await stripe.products.create({
+        name: req.body.name,
+    });
+    // create price and bind it to product
+    const price = await stripe.prices.create({
+        unit_amount: req.body.unit_amount,
+        currency: 'eur',
+        recurring: { interval: 'month' },
+        product: product.id,
+    });
+    res.send(price);
 };
--- routes/pay.ts
+++ routes/pay.ts
@@ -4,8 +4,8 @@ const router = express.Router();
 import * as StripeController from "../controllers/stripe";
 
 router.post("/customer", StripeController.createCustomer); // create customer on stripe
-router.post("/clientSecret", StripeController.clientSecret); // create setup intent and return client secret
-router.post("/subscribe", StripeController.subscribe); // create subscription with payment method from setup intent
-// TODO: route to create product
+router.post("/setupIntent", StripeController.setupIntent); // create setup intent
+router.post("/subscribe", StripeController.subscribe); // creates subscription with payment method from setup intent
+router.post("/ppa", StripeController.ppa); // creates PPA product with price
 
 export default router
