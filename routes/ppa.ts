5aae6bbbd7ef0b496757c2f1d2797e48460859ee
Denis Paluca <ge37dem@mytum.de>
2022-06-26 19:59:36 +0200

    Fix startOfNextMonth not exporting

--- routes/ppa.ts
+++ routes/ppa.ts
@@ -1,10 +1,15 @@
-import express from "express";
+import express, { RequestHandler } from "express";
 import * as PPAController from '../controllers/ppa';
 
 const router = express.Router();
-router.get('/', PPAController.list);
-router.post('/', PPAController.buy)
-router.get('/:id', PPAController.get);
-router.patch('/:id', PPAController.cancel);
+const mockMiddleWare: RequestHandler = (req, res, next) => {
+  (req as any).buyerId = '62b19996acae4107a14709d6';
+  next();
+}
+
+router.get('/', mockMiddleWare, PPAController.list);
+router.post('/', mockMiddleWare, PPAController.buy)
+router.get('/:id', mockMiddleWare, PPAController.get);
+router.patch('/:id', mockMiddleWare, PPAController.cancel);
 
 export default router;
--- utils/time.ts
+++ utils/time.ts
@@ -1,5 +1,4 @@
-
-const startOfNextMonth = (): Date => {
+export const startOfNextMonth = (): Date => {
   const date = new Date();
 
   return new Date(date.getFullYear(), date.getMonth() + 1, 1);
