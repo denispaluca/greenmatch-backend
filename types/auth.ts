f8165cd3ef69aa792cf837c20013b16cb322e52c
aybarsyazici <aybarsyazici@sabanciuniv.edu>
2022-06-28 16:17:15 +0200

    modified registration and login features to suit the needs of the frontend such as including company information & login type

--- types/auth.ts
+++ types/auth.ts
@@ -3,3 +3,11 @@ import { Request } from 'express';
 export interface RequestWithUserId extends Request {
   userId?: string;
 }
+
+export interface Company {
+  country: string;
+  name: string;
+  website: string;
+  imageURL?: string;
+  hrb: string;
+}
