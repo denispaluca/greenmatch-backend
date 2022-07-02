b6c9b9b9f47ebb77fe6f89f03ac4f737cde8f504
Sebastian Schramm <s.schramm@tum.de>
2022-07-02 12:59:38 +0200

    feat: added stripeCustId to user model and stripePriceId to ppa model

--- services/ppa.ts
+++ services/ppa.ts
@@ -2,6 +2,11 @@ import PowerPlantModel from "../models/powerplant";
 import PPAModel from "../models/ppa"
 import { PPABuy, PPAQuery } from "../types/ppa"
 import { startOfNextMonth } from "../utils/time";
+import dotenv from 'dotenv';
+
+// load stripe
+dotenv.config();
+const stripe = require('stripe')(process.env.STRIPE_SK);
 
 export const list = (userId: string, role: 'supplier' | 'buyer', powerplantId?: string) => {
   const query: PPAQuery = {}
@@ -54,7 +59,6 @@ const durationMap = {
 export const buy = async (buyerId: string, buyOrder: PPABuy) => {
   const { powerplantId, amount, duration } = buyOrder;
 
-
   const powerplant = await PowerPlantModel.findOneAndUpdate(
     {
       _id: powerplantId,
@@ -74,12 +78,28 @@ export const buy = async (buyerId: string, buyOrder: PPABuy) => {
     throw Error('Could update the Power Plant');
   }
 
+  // stripe: create product
+  const product = await stripe.products.create({
+    name: 'PPA BuyerID: ' + buyerId,
+  });
+
+  // stripe: create price and bind it to product
+  const price = await stripe.prices.create({
+    unit_amount: Math.ceil((powerplant.price * amount * duration) / (12 * duration)),
+    currency: 'eur',
+    recurring: { interval: 'month' },
+    product: product.id,
+  });
+
+  // create subscription
+
   const ppa = await PPAModel.create({
     buyerId,
     ...buyOrder,
     supplierId: powerplant.supplierId,
     price: powerplant.price,
-    startDate: startOfNextMonth()
+    startDate: startOfNextMonth(),
+    stripePriceId: price.id
   });
 
   return ppa.toObject();
