f9b2df714ea0a37a226f42240bb849cd6e6e8c72
Sebastian Schramm <s.schramm@tum.de>
2022-07-02 15:40:54 +0200

    fix: subscription as service

--- utils/time.ts
+++ utils/time.ts
@@ -3,3 +3,8 @@ export const startOfNextMonth = (): Date => {
 
   return new Date(date.getFullYear(), date.getMonth() + 1, 1);
 }
+
+export const getEndDate = (duration: number): Date => {
+  const date = new Date();
+  return new Date(date.getFullYear() + duration, date.getMonth() + 1, 0);
+};
