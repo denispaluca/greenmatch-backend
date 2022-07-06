e4d006c7a3cb93ec9b9a9d004c8436504f5549d0
Sebastian Schramm <s.schramm@tum.de>
2022-07-06 14:25:55 +0200

    fix: changed username to email

--- routes/auth.ts
+++ routes/auth.ts
@@ -6,7 +6,7 @@ import * as AuthController from "../controllers/auth";
 
 router.post("/login", AuthController.login); // login
 router.post("/register", AuthController.register); // register a new user
-router.get("/me", AuthMiddleware.checkAuthentication, AuthController.me); // get own username, requires a logged in user
+router.get("/me", AuthMiddleware.checkAuthentication, AuthController.me); // get own email, requires a logged in user
 router.get("/logout", AuthMiddleware.checkAuthentication, AuthController.logout); // logout user
 router.get("/setupIntent", AuthMiddleware.checkAuthentication, AuthController.setupIntent); // create setup intent
 
--- routes/username.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as UnameController from "../controllers/username";
-
-const router = express.Router();
-
-router.get("/:id", UnameController.checkAvailability); // check whether username is already in use or not
-
-export default router
