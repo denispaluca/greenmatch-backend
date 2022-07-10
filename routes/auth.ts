6918056a94f16072d4e610f1cd684a58ef84a085
Jan Martin Vogt <jm.vogt@tum.de>
2022-07-10 15:28:43 +0200

    fix: change logout endpoint from get to post

--- routes/auth.ts
+++ routes/auth.ts
@@ -7,7 +7,7 @@ import * as AuthController from "../controllers/auth";
 router.post("/login", AuthController.login); // login
 router.post("/register", AuthController.register); // register a new user
 router.get("/me", AuthMiddleware.checkAuthentication, AuthController.me); // get own email, requires a logged in user
-router.get("/logout", AuthMiddleware.checkAuthentication, AuthController.logout); // logout user
+router.post("/logout", AuthMiddleware.checkAuthentication, AuthController.logout); // logout user
 router.get("/setupIntent", AuthMiddleware.checkAuthentication, AuthController.setupIntent); // create setup intent
 
 export default router;
